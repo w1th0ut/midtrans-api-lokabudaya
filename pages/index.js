b50a010d5085d56e4e7c667515ec25170dac1e17
Bagas Rizki <bagas.gunardi67@gmail.com>
2025-06-06 16:49:02 +0700

    edit

--- pages/index.js
+++ pages/index.js
@@ -107,7 +107,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go nextjs.org →
         </a>
       </footer>
     </div>
